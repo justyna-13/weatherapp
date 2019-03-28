929a450eaa18f54d957cc963eb63777fd07bd1af
marta <marta97chrzanowska@gmail.com>
2019-03-28 18:06:23 +0100

    Create Left Bar

--- main.js
+++ main.js
@@ -83,3 +83,14 @@ const currentWeatherWroDisplay = async () => {
 
 currentWeatherWroDisplay();
 forecastWroDisplay();
+/*Left bar*/
+function iconClick(e) {
+    if (e.target.getAttribute("id") === "icon"){
+            for (i=0; i<6; i++){
+            e.target.parentNode.children[i].setAttribute("style", "color: rgba(255, 255, 255, 0.719);");
+            }
+            e.target.setAttribute("style", "color: rgba(233, 203, 37, 0.76);");
+        }
+}
+
+document.addEventListener("click", iconClick, false);
